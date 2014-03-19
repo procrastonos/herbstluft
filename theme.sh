449b4dc422d4fa70f295c08146b179f4b20d2948
procrastonos <procrastonos@online.de>
2014-03-19 07:25:43 +0100

    Move color and geometry settings to theme script

---
+++ theme.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#-colors-----------------------------------------------------------------------
+
+hc set frame_bg_active_color        '#000000'
+hc set frame_bg_normal_color        '#000000'
+hc set frame_border_active_color    '#000000'
+hc set frame_border_inner_color     '#000000'
+hc set frame_border_normal_color    '#000000'
+hc set window_border_active_color   '#000000'
+hc set window_border_inner_color    '#000000'
+hc set window_border_normal_color   '#000000'
+hc set window_border_urgent_color   '#000000'
+
+#-geometry---------------------------------------------------------------------
+
+hc set frame_border_inner_width     0
+hc set frame_border_width           0
+hc set frame_gap                    0
+hc set frame_padding                0
+hc set frame_transparent_width      0
+hc set window_border_inner_width    0
+hc set window_border_width          0
+hc set window_gap                   0
